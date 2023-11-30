1411f850af6869203709c5dcb05df8dc134e2fb7
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-30 15:39:29 +0300

    initial commit

---
+++ Adoba/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<template>
+  <main data-cursor="-exclusion">
+    <section class="section home-hero">
+        <div class="home-hero__wrapper">
+            <div class="home-hero__img-wrapper">
+                <div class="home-hero__overlay"></div>
+                <img class="home-hero__img" src="/Header.jpg" alt="Adoba Hero img">
+            </div>
+            <div class="home-hero__content-wrapper">
+                <div class="home-hero__title-wrapper">
+                    <h1 class="u-hidden-visually">Avec la 3D dessinons ensemble l'architecture de demain</h1>
+                    <div class="home-hero__title home-hero__title--1">Avec la 3D</div>
+                    <div class="home-hero__title home-hero__title--2">dessinons ensemble</div>
+                    <div class="home-hero__title home-hero__title--3">l'architecture de demain</div>
+                </div>
+                <div class="home-hero__button-wrapper">
+                    <button class="button-primary">
+                        <span>Contactez-nous</span>
+                    </button>
+                </div>
+                <div class="home-hero__scroll-wrapper">
+                    <span>Scroll</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
